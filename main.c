5b695a8e6b55679b5edc863b32be3db8c8162af3
Malte_R <82702984+mrieck19@users.noreply.github.com>
2023-06-02 16:35:32 +0200

    Button Interrupt

--- main.c
+++ main.c
@@ -17,58 +17,70 @@
 
 // Interrupt Service Routine for Timer/Counter1 Overflow
 ISR(TIMER1_OVF_vect) {
-    // Disable the LED on Port C4
-    DISABLE_BUZZER;
+   
+    DISABLE_BUZZER;  // Disable the LED on Port C4
     
-    //Global Interrupt disable
-    cli();
+    TCCR1B &= ~(1 << CS12) & ~(1 << CS10); // Clear prescaler
+}
+
+ISR(PCINT2_vect)
+{
+    // Check if the button on PD1 is pressed
+    if (PIND & (1 << PD1)) {
+        ENABLE_BUZZER;  // Enable the Buzzer
+        resetTimer1();  // Reset the timer/counter1
+    }
 }
 
 void initializeButton(){
     DDRD &= ~(1 << PD1);  // Set PB7 as input pin (Button)
     PORTD |= (1 << PD1);  // Enable pull-up resistor on PB7
+    // Enable PCINT17 interrupt on Port D
+    PCICR |= (1 << PCIE2);
+    PCMSK2 |= (1 << PCINT17);
+    sei();
 }
+
+/*void initializeButton(){
+    DDRD &= ~(1 << PD1);  // Set PB7 as input pin (Button)
+    
+    PORTD |= (1 << PD1);  // Enable pull-up resistor on PB7
+}*/
 
 void initializeBuzzer(){
     DDRC |= (1 << PC4);  // Set Port C Pin 4 (PC4) as output (BUZZER)
 }
 
 
-
 void initializeTimer1(){
-    // Set Timer/Counter Control Register A (TCCR1B) and Timer/Counter Control Register B (TCCR1B)
-    // to configure the timer
-    TCCR1B |= (1 << CS12) | (1 << CS10); // Set prescaler to 1024
+    TIMSK1 |= (1 << TOIE1); // Enable Timer/Counter1 Overflow Interrupt
 
-    // Enable Timer/Counter1 Overflow Interrupt
-    TIMSK1 |= (1 << TOIE1);
-
-    // Set initial value of Timer/Counter1
-    TCNT1 = 0;
+    TCNT1 = 0; // Set initial value of Timer/Counter1
     
-    
+    sei(); // Enable global interrupts
 }
 
 void resetTimer1(){
-     // Reset the Timer/Counter1
-            TCNT1 = 0;
-     // Enable global interrupts
-            sei();       
+     
+    TCNT1 = 0;  // Reset the Timer/Counter1
+    
+    TCCR1B |= (1 << CS12) | (1 << CS10); // Reset prescaler to 1024
 }
 
 int main() {
-    // Initialize the ports
+    
+    // Initialize Functions
     initializeTimer1();
     initializeButton();
     initializeBuzzer();
 
     while (1) {
-        // Check if the button on PC1 is pressed
-        if (BUTTON_PRESS) {
-            // Enable the LED on PC4
-            ENABLE_BUZZER;
+        
+        /*if (BUTTON_PRESS) { // Check if the button is pressed
+            
+            ENABLE_BUZZER;  // Enable the Buzzer
            
-            resetTimer1();
-        }
+            resetTimer1();  // Reset the timer/counter1
+        }*/
     }
 }
