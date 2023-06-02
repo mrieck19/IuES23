e93b502e24b4a360c0bf71b78760072c1c9add0b
Malte_R <82702984+mrieck19@users.noreply.github.com>
2023-06-02 15:52:28 +0200

    Refactoring

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 
 #define F_CPU 16000000UL
 #define BUTTON_PRESS (! (PIND & (1<<PIND1)))
+#define ENABLE_BUZZER PORTC |= (1 << PC4)
+#define DISABLE_BUZZER PORTC &= ~(1 << PC4)
 
 
 #include <avr/io.h>
@@ -16,20 +18,25 @@
 // Interrupt Service Routine for Timer/Counter1 Overflow
 ISR(TIMER1_OVF_vect) {
     // Disable the LED on Port C4
-    PORTC &= ~(1 << PC4);
-   
-  
+    DISABLE_BUZZER;
+    
+    //Global Interrupt disable
+    cli();
 }
 
-void init(void){
-    // Set Port C Pin 4 (PC4) as output (BUZZER)
-    DDRC |= (1 << PC4);
-    
+void initializeButton(){
     DDRD &= ~(1 << PD1);  // Set PB7 as input pin (Button)
     PORTD |= (1 << PD1);  // Enable pull-up resistor on PB7
-  
-    TCCR1A = 0;  
-    // Set Timer/Counter Control Register A (TCCR1A) and Timer/Counter Control Register B (TCCR1B)
+}
+
+void initializeBuzzer(){
+    DDRC |= (1 << PC4);  // Set Port C Pin 4 (PC4) as output (BUZZER)
+}
+
+
+
+void initializeTimer1(){
+    // Set Timer/Counter Control Register A (TCCR1B) and Timer/Counter Control Register B (TCCR1B)
     // to configure the timer
     TCCR1B |= (1 << CS12) | (1 << CS10); // Set prescaler to 1024
 
@@ -39,22 +46,29 @@ void init(void){
     // Set initial value of Timer/Counter1
     TCNT1 = 0;
     
-    // Enable global interrupts
-    sei();
+    
+}
+
+void resetTimer1(){
+     // Reset the Timer/Counter1
+            TCNT1 = 0;
+     // Enable global interrupts
+            sei();       
 }
 
 int main() {
     // Initialize the ports
-    init();
+    initializeTimer1();
+    initializeButton();
+    initializeBuzzer();
 
     while (1) {
         // Check if the button on PC1 is pressed
         if (BUTTON_PRESS) {
             // Enable the LED on PC4
-            PORTC |= (1 << PC4);
-
-            // Reset the Timer/Counter1
-            TCNT1 = 0;
+            ENABLE_BUZZER;
+           
+            resetTimer1();
         }
     }
 }
