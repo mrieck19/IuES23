b4fa7989e1fb2ea95de86370470f8ee0613a52d1
alkl58 <alkl58@protonmail.com>
2023-06-05 16:15:32 +0200

    Cleanup + Documentation

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
  */
 
 #define F_CPU 16000000UL
-#define BUTTON_PRESS (! (PIND & (1<<PIND1)))
+#define BUTTON_PRESS (PIND & (1<<PIND1))
 #define ENABLE_BUZZER PORTC |= (1 << PC4)
 #define DISABLE_BUZZER PORTC &= ~(1 << PC4)
 
@@ -23,51 +23,47 @@ void resetTimer1() {
 
 // Interrupt Service Routine for Timer/Counter1 Overflow
 ISR(TIMER1_OVF_vect) {
+
+    // Increment overflow counter
     overflow_counter++;
     
+    // If 5 seconds (interrupts)
     if (overflow_counter >= 5){
-        DISABLE_BUZZER;  // Disable the LED on Port C4
+        DISABLE_BUZZER;         // Disable the LED on Port C4
     
         TCCR1B &= ~(1 << CS12) & ~(1 << CS10); // Clear prescaler
         
-        overflow_counter = 0; // Reset overflow counter
+        overflow_counter = 0;   // Reset overflow counter
     }
     
     resetTimer1();
 }
 
+// Interrupt Service Routine for PCINT17 (PD1)
 ISR(PCINT2_vect) {
     // Check if the button on PD1 is pressed
-    if (PIND & (1 << PD1)) {
-        ENABLE_BUZZER;  // Enable the Buzzer
-        resetTimer1();  // Reset the timer/counter1
+    if (BUTTON_PRESS) {
+        ENABLE_BUZZER;          // Enable the Buzzer
+        resetTimer1();          // Reset the timer/counter1
     }
 }
 
 void initializeButton() {
-    DDRD &= ~(1 << PD1);  // Set PD1 as input pin (Button)
-    PORTD |= (1 << PD1);  // Enable pull-up resistor on PD1
+    DDRD &= ~(1 << PD1);        // Set PD1 as input pin (Button)
+    PORTD |= (1 << PD1);        // Enable pull-up resistor on PD1
     // Enable PCINT17 interrupt on Port D
-    PCICR |= (1 << PCIE2);
-    PCMSK2 |= (1 << PCINT17);
-    sei();
+    PCICR |= (1 << PCIE2);      // Enable Pin Change Interrupt of PCIE2 (PCINT23..16)
+    PCMSK2 |= (1 << PCINT17);   // Enable PCINT17 (PD1) Inerrupt in Mask Register
 }
 
-/*void initializeButton(){
-    DDRD &= ~(1 << PD1);  // Set PB7 as input pin (Button)
-    
-    PORTD |= (1 << PD1);  // Enable pull-up resistor on PB7
-}*/
-
 void initializeBuzzer() {
-    DDRC |= (1 << PC4);  // Set Port C Pin 4 (PC4) as output (BUZZER)
+    DDRC |= (1 << PC4);         // Set Port C Pin 4 (PC4) as output (BUZZER)
 }
 
 void initializeTimer1() {
-    TIMSK1 |= (1 << TOIE1); // Enable Timer/Counter1 Overflow Interrupt
+    TIMSK1 |= (1 << TOIE1);     // Enable Timer/Counter1 Overflow Interrupt
+    TCNT1 = 49910;              // Set initial value of Timer1, creates interrupt every 1s
 
-    TCNT1 = 49910; // Set initial value of Timer1, creates interrupt every 1s
-    
     sei(); // Enable global interrupts
 }
 
@@ -79,12 +75,6 @@ int main() {
     initializeBuzzer();
 
     while (1) {
-        
-        /*if (BUTTON_PRESS) { // Check if the button is pressed
-            
-            ENABLE_BUZZER;  // Enable the Buzzer
-           
-            resetTimer1();  // Reset the timer/counter1
-        }*/
+
     }
 }
