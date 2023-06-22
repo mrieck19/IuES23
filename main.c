9f027ca68cc32b3a124feab3cdea3535cdf35119
alkl58 <alkl58@protonmail.com>
2023-06-22 15:09:43 +0200

    Add Base functionality

--- main.c
+++ main.c
@@ -9,36 +9,93 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "dht11.h"
 #include "timer1.h"
 #include "buzzer1.h"
 #include "button.h"
-//#include "tft.h"
+#include "tft.h"
 #include "spi.h"
 #include "uart.h"
 
-int main() {
+void resetDisplay() {
+    uint16_t x = 0;
     
+    // Reset Background to White
+    for (x = 0; x < 23232; x++) {
+        SPISend8Bit(0xFF);
+        SPISend8Bit(0xFF);
+    } 
+}
+
+
+int main() {
+
     // Initialize Functions
     initializeTimer1();
     initializeButton();
     initializeBuzzer();
+    initializeDHT11();
     initializeUART();
-    
-    sei();                                      // Enable Global Interrupts
-    
-    while (1) {
+    SPI_init();
+    Display_init();
 
+    resetDisplay();
+
+    sei();                                      // Enable Global Interrupts
+
+    uint8_t humidity, temperature;
+    uint8_t last_mqtt_window_status = 0;
+
+    while (1) {
+        
+        // Send DHT11 Data to MQTT
+        char tempOut[25];
+        dht11_getData(&humidity, &temperature);
+        sprintf(tempOut, ">DHT%d.0:%d.0<", temperature, humidity);
+        sendStringUART(tempOut);
+        
+        _delay_ms(5000);
+        
+        // Send switch Status to MQTT
+        if (BUTTON_PRESS) {
+            // Window Closed (0)
+            if (last_mqtt_window_status == 1) {
+                sendStringUART(">BTN0<");
+            }
+
+            last_mqtt_window_status = 0;
+        }
+        else
+        {
+            // Window Open (1)
+            if (last_mqtt_window_status == 0) {
+                sendStringUART(">BTN1<");
+            }
+
+            last_mqtt_window_status = 1;
+        }
+        
+        resetDisplay();
+        
+        char displayTemp[6];
+        sprintf(displayTemp, "%d.0°C", temperature);
+        TFT_Print(&displayTemp[0], 5, 5, 4, 0x0000, 0xFFFF, 2);
+        
+        char displayHumid[4];
+        sprintf(displayHumid, "%d%%", humidity);
+        TFT_Print(&displayHumid[0], 5, 40, 4, 0x0000, 0xFFFF, 2);
+        
+        _delay_ms(30000);
     }
 }
-/*
-int initialiseDisplay() {
 
-    SPI_init();
-    TFT_init();
-}
-*/
+
 // Interrupt Service Routine for Timer/Counter1 Overflow
 volatile uint8_t overflow_counter = 0;
 ISR(TIMER1_OVF_vect) {
